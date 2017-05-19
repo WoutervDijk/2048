7986749358161fd0d25aef2e548c6013c85338a3
wouter van dijk <wouterrobot@hotmail.nl>
2017-05-19 03:14:14 -0700

    Added a link with an idea

---
+++ 2048/AllBitmapImages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+    public static class AllBitmapImages
+    {
+        //https://msdn.microsoft.com/en-us/library/aa970269(v=vs.110).aspx
+        //Add all the bitmapimages with resource and check if it works
+    }
+}
