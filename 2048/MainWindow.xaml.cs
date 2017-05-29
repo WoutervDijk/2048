e4ce71a3ba2c9f2132b06629d0f4d8dfbeb87c66
wouter van dijk <wouterrobot@hotmail.nl>
2017-05-29 03:12:25 -0700

    Fatal error on startup on line 53.

--- 2048/MainWindow.xaml.cs
+++ 2048/MainWindow.xaml.cs
@@ -20,10 +20,10 @@ namespace _2048
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int[,] numbers;
+        public Image[,] numbers;
         public MainWindow()
         {
-            numbers = new int[3, 3];
+            IntializeArray();
             InitializeComponent();
             //Place_0_0.Source = new BitmapImage(new Uri(@"2048/tail.png", UriKind.RelativeOrAbsolute));
         }
@@ -31,12 +31,37 @@ namespace _2048
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Place_0_0.Source = AllBitmapImages.firstPic;
-            
+            var x = GetRandomPlace();
+            numbers[x.Item1, x.Item2].Source = AllBitmapImages.firstPic;
         }
         private Tuple<int, int> GetRandomPlace()
         {
             Random rand = new Random();
-            rand.Next(0, 3);
+            
+            int a = rand.Next(0, 3);
+            int b = rand.Next(0, 3);
+            var tuble = new Tuple<int, int>(a, b);
+            return tuble;
+        }
+        private void IntializeArray()
+        {
+            numbers = new Image[3, 3];
+            numbers[0, 0] = Place_0_0;
+            numbers[0, 1] = Place_0_1;
+            numbers[0, 2] = Place_0_2;
+            numbers[0, 3] = Place_0_3;
+            numbers[1, 0] = Place_1_0;
+            numbers[1, 1] = Place_1_1;
+            numbers[1, 2] = Place_1_2;
+            numbers[1, 3] = Place_1_3;
+            numbers[2, 0] = Place_2_0;
+            numbers[2, 1] = Place_2_1;
+            numbers[2, 2] = Place_2_2;
+            numbers[2, 3] = Place_2_3;
+            numbers[3, 0] = Place_3_0;
+            numbers[3, 1] = Place_3_1;
+            numbers[3, 2] = Place_3_2;
+            numbers[3, 3] = Place_3_3;
         }
     }
 }
