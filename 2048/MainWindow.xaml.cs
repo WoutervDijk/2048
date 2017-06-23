08e9ace7ab156f6b9d4be9661e24d85946fae17d
wouter van dijk <wouterrobot@hotmail.nl>
2017-06-23 02:05:31 -0700

    Bijna alles af!!!! Alleen nog score en nog ff mooi maken en ervoor zorgen dat restart werkt en nog iets

--- 2048/MainWindow.xaml.cs
+++ 2048/MainWindow.xaml.cs
@@ -22,20 +22,22 @@ namespace _2048
     {
         public Image[,] numbers;
         private bool isPressed = false;
-
+        public bool gameOver;
         public MainWindow()
         {
             InitializeComponent();
             AllBitmapImages.IntializeImages();
             numbers = new Image[4, 4] { { Place_0_0, Place_0_1, Place_0_2, Place_0_3 }, { Place_1_0, Place_1_1, Place_1_2, Place_1_3 }, { Place_2_0, Place_2_1, Place_2_2, Place_2_3 }, { Place_3_0, Place_3_1, Place_3_2, Place_3_3 } };
             AddRandom2Place();
+            gameOver = false;
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.Key == Key.Up && !isPressed)
+            if (e.Key == Key.Up && !isPressed && !gameOver)
             {
-                Image[,] currentGameState = numbers;
+                ImageSource[,] currentGameState = new ImageSource[4, 4];
+                CopyArraySource(numbers, currentGameState);
                 isPressed = true;
                 for (int j = 0; j < 4; j++)
                 {
@@ -43,75 +45,29 @@ namespace _2048
                     {
                         for (int k = i + 1; k < 4; k++)
                         {
-                            if (numbers[k,j].Source == null)
+                            if (numbers[k, j].Source == null)
                             {
                                 continue;
                             }
-                            else if (numbers[k,j].Source == numbers[i,j].Source)
+                            else if (numbers[k, j].Source == numbers[i, j].Source)
                             {
-                                numbers[i,j].Source = OneNumberHigher(numbers[i,j].Source);
+                                numbers[i, j].Source = OneNumberHigher(numbers[i, j].Source);
                                 //iScore += numbers[i][j];
-                                numbers[k,j].Source = null;
+                                numbers[k, j].Source = null;
                                 //bAdd = true;
                                 break;
                             }
                             else
                             {
-                                if (numbers[i,j].Source == null && numbers[k,j].Source != null)
+                                if (numbers[i, j].Source == null && numbers[k, j].Source != null)
                                 {
-                                    numbers[i,j].Source = numbers[k,j].Source;
-                                    numbers[k,j].Source = null;
+                                    numbers[i, j].Source = numbers[k, j].Source;
+                                    numbers[k, j].Source = null;
                                     i--;
                                     //bAdd = true;
                                     break;
                                 }
-                                else if (numbers[i,j].Source != null)
-                                {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                }
-                if(CheckIfGameChanges(numbers,currentGameState))
-                {
-                    AddRandom2Place();
-                }
-                
-            }
-            if(e.Key == Key.Right && !isPressed)
-            {
-                Image[,] currentGameState = numbers;
-                isPressed = true;
-                for (int i = 0; i < 4; i++)
-                {
-                    for (int j = 3; j >= 0; j--)
-                    {
-                        for (int k = j - 1; k >= 0; k--)
-                        {
-                            if (numbers[i,k].Source == null)
-                            {
-                                continue;
-                            }
-                            else if (numbers[i,k].Source == numbers[i,j].Source)
-                            {
-                                numbers[i,j].Source = OneNumberHigher(numbers[i,j].Source);
-                                //iScore += numbers[i][j];
-                                numbers[i,k].Source = null;
-                                //bAdd = true;
-                                break;
-                            }
-                            else
-                            {
-                                if (numbers[i,j].Source == null && numbers[i,k].Source != null)
-                                {
-                                    numbers[i,j].Source = numbers[i,k].Source;
-                                    numbers[i,k].Source = null;
-                                    j++;
-                                    //bAdd = true;
-                                    break;
-                                }
-                                else if (numbers[i,j].Source != null)
+                                else if (numbers[i, j].Source != null)
                                 {
                                     break;
                                 }
@@ -123,10 +79,67 @@ namespace _2048
                 {
                     AddRandom2Place();
                 }
+                if(IsGameOver(numbers))
+                {
+                    gameOver = true;
+                    MessageBox.Show("Lol u lost dickhead");
+                }
             }
-            if (e.Key == Key.Down && !isPressed)
+            if (e.Key == Key.Right && !isPressed && !gameOver)
             {
-                Image[,] currentGameState = numbers;
+                ImageSource[,] currentGameState = new ImageSource[4, 4];
+                CopyArraySource(numbers, currentGameState);
+                isPressed = true;
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 3; j >= 0; j--)
+                    {
+                        for (int k = j - 1; k >= 0; k--)
+                        {
+                            if (numbers[i, k].Source == null)
+                            {
+                                continue;
+                            }
+                            else if (numbers[i, k].Source == numbers[i, j].Source)
+                            {
+                                numbers[i, j].Source = OneNumberHigher(numbers[i, j].Source);
+                                //iScore += numbers[i][j];
+                                numbers[i, k].Source = null;
+                                //bAdd = true;
+                                break;
+                            }
+                            else
+                            {
+                                if (numbers[i, j].Source == null && numbers[i, k].Source != null)
+                                {
+                                    numbers[i, j].Source = numbers[i, k].Source;
+                                    numbers[i, k].Source = null;
+                                    j++;
+                                    //bAdd = true;
+                                    break;
+                                }
+                                else if (numbers[i, j].Source != null)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (CheckIfGameChanges(numbers, currentGameState))
+                {
+                    AddRandom2Place();
+                }
+                if (IsGameOver(numbers))
+                {
+                    gameOver = true;
+                    MessageBox.Show("Lol u lost dickhead");
+                }
+            }
+            if (e.Key == Key.Down && !isPressed && !gameOver)
+            {
+                ImageSource[,] currentGameState = new ImageSource[4, 4];
+                CopyArraySource(numbers, currentGameState);
                 isPressed = true;
                 for (int j = 0; j < 4; j++)
                 {
@@ -168,11 +181,17 @@ namespace _2048
                 {
                     AddRandom2Place();
                 }
+                if (IsGameOver(numbers))
+                {
+                    gameOver = true;
+                    MessageBox.Show("Lol u lost dickhead");
+                }
             }
-            if (e.Key == Key.Left && !isPressed)
+            if (e.Key == Key.Left && !isPressed && !gameOver)
             {
-                ImageSource[,] currentGameState = new ImageSource[4,4];
-                Array.Copy(numbers, currentGameState, 16);
+                ImageSource[,] currentGameState = new ImageSource[4, 4];
+                CopyArraySource(numbers, currentGameState);
+
                 isPressed = true;
                 for (int i = 0; i < 4; i++)
                 {
@@ -186,7 +205,7 @@ namespace _2048
                             }
                             else if (numbers[i, k].Source == numbers[i, j].Source)
                             {
-                                numbers[i, j].Source = OneNumberHigher(numbers[i,j].Source);
+                                numbers[i, j].Source = OneNumberHigher(numbers[i, j].Source);
                                 //iScore += numbers[i][j];
                                 numbers[i, k].Source = null;
                                 //bAdd = true;
@@ -213,6 +232,11 @@ namespace _2048
                 if (CheckIfGameChanges(numbers, currentGameState))
                 {
                     AddRandom2Place();
+                }
+                if (IsGameOver(numbers))
+                {
+                    gameOver = true;
+                    MessageBox.Show("Lol u lost dickhead");
                 }
             }
         }
@@ -271,6 +295,7 @@ namespace _2048
                     reDoLoop = true;
             } while (reDoLoop);
         }
+        //returns an ImageSource die een niveau hoger is
         private BitmapImage OneNumberHigher(ImageSource current)
         {
             if (current == AllBitmapImages.allPics[0])
@@ -299,16 +324,46 @@ namespace _2048
                 return AllBitmapImages.allPics[12];
             else
                 return AllBitmapImages.allPics[0];
-            
+
         }
-        private bool CheckIfGameChanges(Image[,] current, Image[,] old)
+        //maakt een copy van het originele veld
+        private void CopyArraySource(Image[,] arraytoCopy, ImageSource[,] copiedArray)
         {
-            if(numbers[0,0].Source == old[0,0].Source && numbers[0, 1].Source == old[0, 1].Source && numbers[0, 2].Source == old[0, 2].Source && numbers[0, 3].Source == old[0, 3].Source && numbers[1, 0].Source == old[1, 0].Source && numbers[1, 1].Source == old[1, 1].Source && numbers[1, 2].Source == old[1, 2].Source && numbers[1, 3].Source == old[1, 3].Source && numbers[2, 0].Source == old[2, 0].Source && numbers[2, 1].Source == old[2, 1].Source && numbers[2, 2].Source == old[2, 2].Source && numbers[2, 3].Source == old[2, 3].Source && numbers[3, 0].Source == old[3, 0].Source && numbers[3, 1].Source == old[3, 1].Source && numbers[3, 2].Source == old[3, 2].Source && numbers[3, 3].Source == old[3, 3].Source)
+            copiedArray[0, 0] = arraytoCopy[0, 0].Source;
+            copiedArray[0, 1] = arraytoCopy[0, 1].Source;
+            copiedArray[0, 2] = arraytoCopy[0, 2].Source;
+            copiedArray[0, 3] = arraytoCopy[0, 3].Source;
+            copiedArray[1, 0] = arraytoCopy[1, 0].Source;
+            copiedArray[1, 1] = arraytoCopy[1, 1].Source;
+            copiedArray[1, 2] = arraytoCopy[1, 2].Source;
+            copiedArray[1, 3] = arraytoCopy[1, 3].Source;
+            copiedArray[2, 0] = arraytoCopy[2, 0].Source;
+            copiedArray[2, 1] = arraytoCopy[2, 1].Source;
+            copiedArray[2, 2] = arraytoCopy[2, 2].Source;
+            copiedArray[2, 3] = arraytoCopy[2, 3].Source;
+            copiedArray[3, 0] = arraytoCopy[3, 0].Source;
+            copiedArray[3, 1] = arraytoCopy[3, 1].Source;
+            copiedArray[3, 2] = arraytoCopy[3, 2].Source;
+            copiedArray[3, 3] = arraytoCopy[3, 3].Source;
+        }
+        //Kijkt of de copy nog hetzelfde is als de current game
+        private bool CheckIfGameChanges(Image[,] current, ImageSource[,] copy)
+        {
+            if (numbers[0, 0].Source == copy[0, 0] && numbers[0, 1].Source == copy[0, 1] && numbers[0, 2].Source == copy[0, 2] && numbers[0, 3].Source == copy[0, 3] && numbers[1, 0].Source == copy[1, 0] && numbers[1, 1].Source == copy[1, 1] && numbers[1, 2].Source == copy[1, 2] && numbers[1, 3].Source == copy[1, 3] && numbers[2, 0].Source == copy[2, 0] && numbers[2, 1].Source == copy[2, 1] && numbers[2, 2].Source == copy[2, 2] && numbers[2, 3].Source == copy[2, 3] && numbers[3, 0].Source == copy[3, 0] && numbers[3, 1].Source == copy[3, 1] && numbers[3, 2].Source == copy[3, 2] && numbers[3, 3].Source == copy[3, 3])
             {
                 return false;
             }
             return true;
         }
-
+        //Controleert of het spel over is
+        private bool IsGameOver(Image[,] gameBoard)
+        {
+            if(gameBoard[0,0].Source != null && gameBoard[0, 1].Source != null && gameBoard[0, 2].Source != null && gameBoard[0, 3].Source != null && gameBoard[1, 0].Source != null && gameBoard[1, 1].Source != null && gameBoard[1, 2].Source != null && gameBoard[1, 3].Source != null && gameBoard[2, 0].Source != null && gameBoard[2, 1].Source != null && gameBoard[2, 2].Source != null && gameBoard[2, 3].Source != null && gameBoard[3, 0].Source != null && gameBoard[3, 1].Source != null && gameBoard[3, 2].Source != null && gameBoard[3, 3].Source != null)
+            {
+                return true;
+            }
+            return false;
+        }
+        
     }
 }
